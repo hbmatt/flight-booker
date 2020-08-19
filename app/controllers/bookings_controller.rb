87018b3bdee6c7b7f3af96c05391ddd5a0e5b1aa
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-19 17:59:38 -0500

    Add Booking functionality

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+  def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = Booking.new
+    @booking.flight_id = params[:flight_id].to_i
+    params[:num_tickets].to_i.times { @booking.passengers.build }
+  end
+
+  def create
+    @flight = Flight.find(booking_params[:flight_id])
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    @passengers = @booking.passengers.all
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(:flight_id, :passenger_id, passengers_attributes: [:name, :email, :flight_id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :airports
-  resources :flights
+  resources :airports, only: [:create]
+  resources :flights, only: [:index, :create]
+  resources :bookings, only: [:new, :create, :show]
   root 'flights#index'
 end
