965c12da9877a93e38f2ed08d72cf560fe810fec
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-21 16:24:40 -0500

    Create confirmation emails

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifs@example.com'
+
+  def thank_you_email
+    @booking = params[:booking]
+    mail(to: @booking.passenger.email, subject: "Booking Confirmation")
+  end
 end
